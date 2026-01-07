fad0aa99cfa7cb46f3d1838142cd880475e52e67
unknown <rajshivam2745@gmail.com>
2026-01-07 17:22:35 +0530

    Home page and animation added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-
+"use client";
 function page() {
-  return <div>
-    <h1 className="text-3xl font-semibold">Event Management App</h1>
-  </div>;
+  return (
+    <>
+      <section>
+        <h1 className="text-center text-5xl">
+          Your One stop Solution for
+          <br />
+          Managing Tech Events
+        </h1>
+        <p className="text-center mt-4 text-2xl">
+          Confereneces | Hackathons | Meetings
+        </p>
+      </section>
+    </>
+  );
 }
 
 export default page;
