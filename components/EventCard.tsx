8c0ac32671cb51c7cd98407b18ff31a062b11b0a
unknown <rajshivam2745@gmail.com>
2026-01-08 23:16:20 +0530

    UI fixes added for homepage

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -21,20 +21,39 @@ function EventCard({
   eventImage,
 }: EventCardProps) {
   return (
-    <div>
+    <div className="mt-4">
       <Link
         className="flex items-center justify-between text-center"
         href={`/events${eventId}`}
       >
         <h3 className="text-3xl font-semibold">{eventTitle}</h3>
-        <h4>{eventDuration}</h4>
 
-        <Image src="/icons/pin.svg" alt="locationIcon" width={20} height={20} />
-        <p>{eventLocation}</p>
+        <div className="flex gap-2 items-center">
+          <Image
+            src="/icons/calendar.svg"
+            alt="calendarImage"
+            width={15}
+            height={15}
+          />
+          <h4>{eventDuration}</h4>
+        </div>
+
+        <div className="flex gap-2">
+          <Image
+            src="/icons/pin.svg"
+            alt="locationIcon"
+            width={15}
+            height={15}
+          />
+          <p>{eventLocation}</p>
+        </div>
       </Link>
 
-      <Image src={eventImage} alt="eventImage" width={250} height={250} />
-      <p>{eventDescription}</p>
+      <div className="flex gap-5 ">
+        <Image src={eventImage} alt="eventImage" width={500} height={250} />
+        <p className="top-2 ">{eventDescription}</p>
+      </div>
+
       <br />
     </div>
   );
