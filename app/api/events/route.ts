017bb042a2b6aa3071147100af7b82c111cfccc0
unknown <rajshivam2745@gmail.com>
2026-01-11 01:24:51 +0530

    cloudinary service added in route.ts and GET route added

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/database";
 import Event from "@/models/EventModel";
 import { generateSlug } from "@/models/EventModel";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   try {
@@ -14,7 +15,7 @@ export async function POST(req: NextRequest) {
     const description = formData.get("description") as string;
     const overview = formData.get("overview") as string;
     const venue = formData.get("venue") as string;
-    const image = formData.get("image") as string;
+    const image = formData.get("image") as File;
     const location = formData.get("location") as string;
     const date = formData.get("date") as string;
     const time = formData.get("time") as string;
@@ -23,15 +24,48 @@ export async function POST(req: NextRequest) {
     const tags = formData.getAll("tags") as string[];
     const audience = formData.get("audience") as string;
     const organizer = formData.get("organizer") as string;
+    const MAX_SIZE_IMAGE: number = 5 * 1024 * 1024;
 
-    const slugGeneration = generateSlug(title);
+    if (!image) {
+      return NextResponse.json(
+        { message: "Please upload image first" },
+        { status: 400 }
+      );
+    }
+
+    if (image.size > MAX_SIZE_IMAGE) {
+      return NextResponse.json({
+        message: "Please upload image less than 5 Mb",
+      });
+    }
+
+    // image conversion to buffer
+    const arrayBuffer = await image.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadImage = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          {
+            resource_type: "image",
+            folder: "eventmanagement",
+          },
+          (error, success) => {
+            if (error) reject(error);
+            resolve(success);
+          }
+        )
+        .end(buffer);
+    });
+
+    const url = (uploadImage as { secure_url: string }).secure_url;
 
     const eventCreated = await Event.create({
       title,
       description,
       overview,
       venue,
-      image,
+      image: url,
       location,
       date,
       time,
@@ -40,7 +74,7 @@ export async function POST(req: NextRequest) {
       tags,
       audience,
       organizer,
-      slug: slugGeneration,
+      slug: generateSlug(title),
     });
 
     if (!eventCreated) {
@@ -67,6 +101,32 @@ export async function POST(req: NextRequest) {
         message: errorMessage,
         error: error instanceof Error ? error.message : String(error),
       },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 }); //ensures latest/ new events are shown first
+    if (!events) {
+      return NextResponse.json(
+        { message: "Oops No events found." },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      {
+        message: "Events fetched successfully",
+        events,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error occured while fetching the events", error },
       { status: 500 }
     );
   }
